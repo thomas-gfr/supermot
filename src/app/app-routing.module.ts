4474edd7400d315e305a4a81c06561a87e3bdf41
thomas-gfr <g.thomas.pro806@gmail.com>
2022-11-08 10:01:44 +0100

    add localstorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: LandingPageComponent
     },
     {
-        path: 'shop/:id',
+        path: 'shop/funnel',
         component: ShoppingFunnelComponent
     }
 ];
